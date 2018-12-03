5280355b70739506534d8770f054839a36ea88d8
vhaisong <vhaisong@126.com>
2018-12-03 09:50:41 +0800

    Spring Cloud -- 服务注册中心 和 服务提供者 （Finchley版本）

---
+++ cloud/finchley-eureka-client/src/main/java/cn/jxdog/miscellaneous/cloud/finchley/eureka/client/Application.java
@@ -0,0 +1,32 @@
+package cn.jxdog.miscellaneous.cloud.finchley.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 服务提供者
+ * @Author vhs
+ * @Date 2018-11-30
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/show")
+    public String show() {
+        return "this is eureka-client, port is " + port;
+    }
+}
